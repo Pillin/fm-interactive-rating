c4e002c9d881bc6e9dfbfaedb13bb50bcecaef39
Pillippa Perez Pons <ignacia.pp@gmail.com>
2022-04-14 18:30:58 -0700

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `simple-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
